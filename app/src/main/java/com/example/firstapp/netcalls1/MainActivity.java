5c4ebda3d25b1913a86a64235ca316b7ecdd2f97
colt005 <rohan.prabhu05@gmail.com>
2018-06-11 21:35:25 +0530

    Added network call in an asynctask

--- app/src/main/java/com/example/firstapp/netcalls1/MainActivity.java
+++ app/src/main/java/com/example/firstapp/netcalls1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.firstapp.netcalls1;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,56 +21,20 @@ import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 public class MainActivity extends AppCompatActivity {
 
 
+    private TextView tvdata;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btnhit = (Button) (findViewById(R.id.btnhit));
-        final TextView tvdata = (TextView) (findViewById(R.id.tvJSONItem));
+        tvdata = (TextView) (findViewById(R.id.tvJSONItem));
 
         btnhit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                HttpURLConnection connection = null;
-                BufferedReader reader = null;
-                try {
 
-                    URL url;
-                    url = new URL("http://my-json-server.typicode.com/colt005/jsondemo/posts/1");
-                    connection = (HttpURLConnection) url.openConnection();
-                    connection.connect();
-
-                    InputStream stream = connection.getInputStream();
-
-                    reader = new BufferedReader(new InputStreamReader(stream));
-                    String line = " ";
-                    StringBuffer buffer = new StringBuffer();
-
-                    while ((line = reader.readLine()) != null) {
-
-                        buffer.append(line);
-
-                    }
-                    tvdata.setText(buffer.toString());
-
-
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    if (connection != null) {
-                        connection.disconnect();
-                    }
-                    try {
-                        if (reader != null) {
-                            reader.close();
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
+                new JSONtask().execute("http://my-json-server.typicode.com/colt005/jsondemo/posts/1");
 
 
             }
@@ -77,4 +42,63 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    public class JSONtask extends AsyncTask<String, String, String>
+    {
+
+
+
+
+        @Override
+        protected String doInBackground(String... urls) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+
+                URL url;
+                url = new URL(urls[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+                String line = " ";
+                StringBuffer buffer = new StringBuffer();
+
+                while ((line = reader.readLine()) != null) {
+
+                    buffer.append(line);
+
+                }
+
+                return buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            tvdata.setText(result);
+        }
     }
+}
